c63be03361f21d50318d573250f619a6e5b398c6
Andrei Smolich <andy.smolich@gmail.com>
2022-02-24 14:57:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+
+  const openBrackets = bracketsConfig.map(elem => elem[0]);
+  const closeBrackets = bracketsConfig.map(elem => elem[1]);
+  const stack = [];
+
+  for(let i = 0; i < str.length; i++) {
+    const currentBracket = str[i];
+    const lastStackBracket = stack[stack.length - 1];
+
+    if(openBrackets.includes(currentBracket)) {
+      if(closeBrackets.includes(lastStackBracket) && currentBracket === lastStackBracket) {
+        stack.pop();
+        continue;
+      }
+      stack.push(str[i]);
+    } else {
+      if(closeBrackets.indexOf(currentBracket) === openBrackets.indexOf(lastStackBracket)) {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length === 0 ? true : false;
 }
